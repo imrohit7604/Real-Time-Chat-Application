26fdb5ccc0f026d071039ba45ba27ce931f64ac7
Mr.NoOne <imrohit7604@gmail.com>
2021-03-30 17:33:01 +0530

    final

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const socket = require("socket.io");
+
+// App setup
 const app = express();
-const server = app.listen(4000, () => {
-  console.log("Listening on port 4000");
+const server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
 });
 
-app.use(express.static("public"));
+// Static files
+app.use(express.static('public'));
 
+// Socket setup & pass server
 const io = socket(server);
+io.on('connection', (socket) => {
 
-io.on("connection", (socket) => {
-  console.log("Made connection , id: ", socket.id);
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
 });
